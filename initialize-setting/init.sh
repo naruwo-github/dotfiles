fa01868178e04317b8e15fd3012328e14400a91b
channaru <narutyan0711@gmail.com>
2022-07-31 21:27:16 +0900

    update asdf setting

--- initialize-setting/init.sh
+++ initialize-setting/init.sh
@@ -6,3 +6,11 @@ brew install fzf
 brew install asdf
 brew install exa
 brew install starship
+
+# Third, install language via asdf
+. /usr/local/opt/asdf/libexec/asdf.sh
+
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+NODE_VERSION=18.6.0
+asdf install nodejs $NODE_VERSION
+asdf global nodejs $NODE_VERSION
